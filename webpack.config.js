09065c10b345ed70438b319844a8004ae41d76cc
Fabian Wittel <fabianwittel@fw-MacBook.fritz.box>
2019-12-20 17:11:40 +0100

    add babel, static link to bundled js

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,15 @@ const webpack = require('webpack')
 
 module.exports = {
 	entry: './src/client/index.js',
+	mode: 'development'
 }
+
+module: {
+            rules: [
+                    {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+                    }
+            ]
+    }
